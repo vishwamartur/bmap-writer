8e0aa500ef2ca29e2e57cbd91190ae335f6a8fa7
Ayoub Zaki <ayoub.zaki@embetrix.com>
2024-11-19 17:33:21 +0100

    add testing

--- bmap-writer-test.sh
+++ bmap-writer-test.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-echo "ok"
+# This script tests the bmap-writer tool
+
+# Create a file with random data
+dd if=/dev/urandom of=test.img bs=1M count=10
+dd if=/dev/urandom of=test.img bs=1M count=2 seek=12 conv=notrunc
+dd if=/dev/urandom of=test.img bs=1M count=5 seek=16 conv=notrunc
+
+# Compress the file with xz and gzip
+xz   -z test.img -c   > test.img.xz
+gzip -9 test.img -c   > test.img.gz
+
+# Create a bmap file
+bmaptool create test.img -o test.img.bmap
+
+# Write the file with bmaptool as reference
+bmaptool copy test.img test.img.out
+
+# Write the file with bmap-writer
+./bmap-writer test.img test.img.bmap test.w.img.out
+cmp test.img.out test.w.img.out
+
+# Write the file with bmap-writer and xz
+./bmap-writer test.img.xz test.img.bmap test.w.img.out
+cmp test.img.out test.w.img.out
+
+# Write the file with bmap-writer and gzip
+./bmap-writer test.img.gz test.img.bmap test.w.img.out
+cmp test.img.out test.w.img.out
